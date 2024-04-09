cf9a8bdf2a0e97d792ca9d71fd71b1a82b84222a
abfiker <new94abemelek@gmail.com>
2024-04-09 20:51:35 +0300

    fakeIn

---
+++ fakeIn.js
@@ -0,0 +1,13 @@
+function fakeBin(x) {
+    let result = ''
+    for (let i in x) {
+        if (x[i] < 5) {
+            result += '0'
+        } else {
+            result += '1'
+        }
+        console.log()
+    }
+    return result
+}
+console.log(fakeBin('45385593107843568'))
