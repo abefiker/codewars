c3572323454c76048a240982f4c4daa48c756af6
abfiker <new94abemelek@gmail.com>
2024-04-10 11:54:45 +0300

    k

---
+++ nospace.js
@@ -0,0 +1,11 @@
+function noSpace(x) {
+    let result = ''
+    for (let p in x) {
+        if (x[p] != ' ') {
+            result += x[p] 
+        } 
+    }
+    console.log(result)
+    
+}
+noSpace('8 j 8   mBliB8g  imjB8B8  jl  B')
